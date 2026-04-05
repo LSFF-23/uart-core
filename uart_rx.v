70ef17d98bdcd45683c0f5f85c4f39834499137a
unknown <luisfelipe.fsk@gmail.com>
2026-04-05 05:18:37 -0300

    feat(): uart receiver (glitched)

---
+++ uart_rx.v
@@ -0,0 +1,116 @@
+module uart_rx (
+    input clk,
+    input rstn,
+    input baud_tick,
+    input rx_pin,
+    output [7:0] rx_data,
+    output rx_done,
+    output frame_error
+);
+
+localparam  IDLE = 3'b000,
+            START = 3'b001,
+            DATA = 3'b010,
+            STOP = 3'b011,
+            ERROR = 3'b100,
+            DONE = 3'b111;
+
+reg [2:0] state, next_state;
+always @(posedge clk, negedge rstn) begin
+    if (!rstn)
+        state <= IDLE;
+    else
+        state <= next_state;
+end
+
+reg rx_sync1, rx_sync2;
+always @(posedge clk) begin
+    if (!rstn) begin
+        rx_sync1 <= 1'b1;
+        rx_sync2 <= 1'b1;
+    end else begin
+        rx_sync1 <= rx_pin;
+        rx_sync2 <= rx_sync1;
+    end
+end
+
+reg [3:0] tick_counter;
+reg [2:0] bit_counter;
+wire end_tick = tick_counter == 4'hf;
+wire middle_tick = tick_counter == 4'h7;
+wire end_bit = bit_counter == 3'h7;
+always @* begin
+    next_state = state;
+    case (state)
+        IDLE: begin
+            if (rx_sync1 == 1'b0 && rx_sync2 == 1'b1)
+                next_state = START;
+        end
+        START: begin
+            if (baud_tick && middle_tick)
+                if (rx_sync2 == 1'b0)
+                    next_state = DATA;
+                else
+                    next_state = IDLE;
+        end
+        DATA: begin
+            if (baud_tick && end_tick && end_bit)
+                next_state = STOP;
+        end
+        STOP: begin
+            if (baud_tick && end_tick)
+                if (rx_sync2 == 1'b1)
+                    next_state = DONE;
+                else
+                    next_state = ERROR;
+        end
+        ERROR, DONE: next_state = IDLE;
+        default: next_state = IDLE;
+    endcase
+end
+
+reg [7:0] data_reg;
+always @(posedge clk, negedge rstn) begin
+    if (!rstn) begin
+        tick_counter <= 4'b0;
+        bit_counter <= 3'b0;
+        data_reg <= 8'b0;
+    end else begin
+        case (state)
+            IDLE: begin
+                if (rx_sync1 == 1'b0 && rx_sync2 == 1'b1) begin
+                    tick_counter <= 4'b0;
+                    bit_counter <= 3'b0;
+                    data_reg <= 8'b0;
+                end
+            end
+            START: begin
+                if (baud_tick) begin
+                    if (middle_tick)
+                        tick_counter <= 4'b0;
+                    else
+                        tick_counter <= tick_counter + 1'b1;
+                end
+            end
+            DATA: begin
+                if (baud_tick) begin
+                    tick_counter <= tick_counter + 1'b1;
+                    if (middle_tick)
+                        data_reg <= {rx_sync2, data_reg[7:1]};
+                    if (end_tick)
+                        bit_counter <= bit_counter + 1'b1;
+                end
+            end
+            STOP: begin
+                if (baud_tick)
+                    tick_counter <= tick_counter + 1'b1;
+            end
+        endcase
+    end
+end
+
+assign rx_data = data_reg;
+assign rx_done = (state == DONE);
+assign frame_error = (state == ERROR);
+
+endmodule
