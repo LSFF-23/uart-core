5497eb6a48a0519bf799fcac074c8ce5dfb1ff18
unknown <luisfelipe.fsk@gmail.com>
2026-06-11 03:39:39 -0300

    fix(): reminder to interface

--- uart_top.sv
+++ uart_top.sv
@@ -4,14 +4,14 @@ module uart_top #(
 ) (
     input logic clk,
     input logic rstn,
-    input logic rx_pin,
     input logic tx_start,
     input logic [7:0] tx_data,
     output logic tx_pin,
+    output logic tx_busy,
+    input logic rx_pin,
     output logic [7:0] rx_data,
     output logic rx_done,
-    output logic frame_error,
-    output logic tx_busy
+    output logic frame_error
 );
 
 logic baud_tick;
