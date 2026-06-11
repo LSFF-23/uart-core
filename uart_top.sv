3b2a1f9d64ac99c71c5fabdc1675b8277d81298d
unknown <luisfelipe.fsk@gmail.com>
2026-06-11 03:02:15 -0300

    fix(): wildcard connection

--- uart_top.sv
+++ uart_top.sv
@@ -19,30 +19,10 @@ logic baud_tick;
 baud_gen #(
     .BAUD_RATE(BAUD_RATE),
     .CLK_FREQUENCY(CLK_FREQUENCY)
-) u_baud_gen (
-    .clk(clk),
-    .rstn(rstn),
-    .baud_tick(baud_tick)
-);
+) u_baud_gen (.*);
 
-uart_rx u_uart_rx (
-    .clk(clk),
-    .rstn(rstn),
-    .baud_tick(baud_tick),
-    .rx_pin(rx_pin),
-    .rx_data(rx_data),
-    .rx_done(rx_done),
-    .frame_error(frame_error)
-);
+uart_rx u_uart_rx (.*);
 
-uart_tx u_uart_tx (
-    .clk(clk),
-    .rstn(rstn),
-    .baud_tick(baud_tick),
-    .tx_start(tx_start),
-    .tx_data(tx_data),
-    .tx_pin(tx_pin),
-    .tx_busy(tx_busy)
-);
+uart_tx u_uart_tx (.*);
 
 endmodule
